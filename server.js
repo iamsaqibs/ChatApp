564ad6018db4d80b71d853a29d440e78ee37f00c
Saqib S <msaqibsaeed4@gmail.com>
2020-08-30 00:37:20 +0500

    WIP

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./models/messages');
+
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const botName = 'Chat Bot';
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+io.on('connection', socket => {
+    socket.emit('message', formatMessage(botName, 'Welcome to chat app'));
+    socket.broadcast.emit('message', formatMessage(botName, 'A new user has joined the chat'));
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+    socket.on('chatMessage', msg => {
+        console.log(msg);
+        io.emit('message', msg); 
+    });
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server Runnng at port ${PORT}`));
+server.listen(PORT, () => console.log(`Server Runnng at port ${PORT}`));
